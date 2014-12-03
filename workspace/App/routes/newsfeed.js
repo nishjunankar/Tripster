bae3a30a7c420512e69df65e789a04beb8ccc697
Nishant Junankar <nishjunankar@gmail.com>
2014-12-03 01:53:12 -0500

    added trips to newsfeed

--- workspace/App/routes/newsfeed.js
+++ workspace/App/routes/newsfeed.js
@@ -7,13 +7,20 @@ var connectData = {
   "database": "c450proj" };
 var oracle =  require("oracle");
 
-var uid = 'lsn';
+var uid = '1';
 /////
 // Query the oracle database, and call output_actors on the results
 //
 // res = HTTP result object sent back to the client
 // name = Name to query for
+function connect_and_query(query) {
+	
+	
+}
+
 function query_db(res) {
+  var photos = null;
+  var trips = null;
   oracle.connect(connectData, function(err, connection) {
     if ( err ) {
     	console.log(err);
@@ -25,11 +32,30 @@ function query_db(res) {
 	  			   [], 
 	  			   function(err, results) {
 	  	    if ( err ) {
-	  	    	console.log(err);
 	  	    } else {
 	  	    	connection.close(); // done with the connection
-	  	    	console.log(results);
-	  	    	output_newsfeed(res, results);
+	  	    	photos = results;
+	  	    	
+	  	    	oracle.connect(connectData, function(err, connection) {
+	  	   	    if ( err ) {
+	  	   	    	console.log(err);
+	  	   	    } else {
+	  	   		  	// selecting rows
+	  	   	    	query = construct_query_friends_trips(uid);
+	  	   		  	connection.execute(query, 
+	  	   		  			   [], 
+	  	   		  			   function(err, results) {
+	  	   		  	    if ( err ) {
+	  	   		  	    	console.log(err);
+	  	   		  	    } else {
+	  	   		  	    	connection.close(); // done with the connection
+	  	   		  	    	trips = results;
+	  	   		  	    	output_newsfeed(res, photos, trips);
+	  	   		  	    }
+	  	   		
+	  	   		  	}); // end connection.execute
+	  	   	    }
+	  	   	  }); // end oracle.connect
 	  	    }
 	
 	  	}); // end connection.execute
@@ -64,16 +90,54 @@ function construct_query_friends_photos(uid) {
 		"ORDER BY TIMES DESC";
 	return query;
 }
+
+function construct_query_friends_trips(uid) {
+	var query="";
+	query += "WITH FRIENDS AS ( \n";
+	query += "SELECT FW.OTHER_FRIEND\n";
+	query += "FROM Users u\n";
+	query += "INNER JOIN Friends_With FW ON FW.Friend = U.u_id\n";
+	query += "WHERE U.u_id = '" + uid + "'\n";
+	query += "),\n";
+	query += "TRIPS_ACCEPTED_BY_FRIENDS AS (\n";
+	query += "SELECT T.T_ID, F.OTHER_FRIEND\n";
+	query += "FROM  Friends F\n";
+	query += "INNER JOIN Invite_Trip IT ON IT.Invited_Users = F.Other_Friend\n";
+	query += "INNER JOIN Trip T ON T.T_ID = IT.T_ID\n";
+	query += "WHERE IT.Accepted = 1\n";
+	query += "AND PRIVACY_FLAG = 0\n";
+	query += "),\n";
+	query += "";
+	query += "TRIPS_CREATED_BY_FRIENDS AS (\n";
+	query += "SELECT T.T_ID, F.OTHER_FRIEND\n";
+	query += "FROM Friends F\n";
+	query += "INNER JOIN Trip T ON T.Creator = F.Other_Friend\n";
+	query += "WHERE PRIVACY_FLAG = 0\n";
+	query += "),\n";
+	query += "";
+	query += "TRIPS_BY_FRIENDS AS (\n";
+	query += "SELECT *\n";
+	query += "FROM TRIPS_CREATED_BY_FRIENDS TCBF UNION  SELECT * FROM TRIPS_ACCEPTED_BY_FRIENDS TABF\n";
+	query += ")\n";
+	query += "";
+	query += "SELECT T.T_ID, TBF.OTHER_Friend AS Friend, T.START_DATE, T.END_DATE, T.Location, T.Name AS Trip_Name, U.First_name, U.Last_name\n";
+	query += "FROM TRIPS_BY_FRIENDS TBF\n";
+	query += "INNER JOIN Trip T ON T.T_ID = TBF.T_ID\n";
+	query += "INNER JOIN Users U ON TBF.Other_Friend = U.U_ID\n";
+
+	return query;
+}
 /////
 // Given a set of query results, output a table
 //
 // res = HTTP result object sent back to the client
 // name = Name to query for
 // results = List object of query results
-function output_newsfeed(res,results) {
+function output_newsfeed(res,photos,trips) {
 	res.render('newsfeed.jade',
 		   { title: "Newsfeed for user ",
-		     photos: results,
+		     photos: photos,
+		     trips:trips,
 		     uid: uid}
 	  );
 }
